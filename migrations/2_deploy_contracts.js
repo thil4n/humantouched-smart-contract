68fe36be646166fdcaa74b4f8a355997fda6831c
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2025-02-08 15:11:51 +0530

    update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Token = artifacts.require("./Token.sol");
 
 module.exports = function (deployer) {
-    deployer.deploy(
-        SimpleStorage,
-        "0x3CE5128475c1d110e6347Ea8C4cf09Aae9eB1506"
-    );
+    deployer.deploy(Token, "0x3CE5128475c1d110e6347Ea8C4cf09Aae9eB1506");
 };
