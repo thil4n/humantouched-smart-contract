e07586ff02479371708869d5cc320675338aa639
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2025-02-15 11:07:14 +0530

    update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Token = artifacts.require("./Token.sol");
 
 module.exports = function (deployer) {
-    deployer.deploy(Token, "0x119813b14d0f7d6FB10334ddE7FD0Df2C90977e0");
+    deployer.deploy(Token, "0x627306090abaB3A6e1400e9345bC60c78a8BEf57");
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,10 @@
 module.exports = {
     networks: {
         development: {
-            host: "127.0.0.1",
+            host: "77.37.121.45",
             port: 8545,
             network_id: "*",
-            from: "0x119813b14d0f7d6FB10334ddE7FD0Df2C90977e0",
+            from: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
         },
     },
     compilers: {
